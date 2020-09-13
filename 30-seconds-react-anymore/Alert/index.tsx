3f2f722e9bd6d71da0c24e14e27815591fe56ab4
klay <klaaay1998@163.com>
2020-09-13 21:52:13 +0800

    Alert

---
+++ 30-seconds-react-anymore/Alert/index.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from "react";
+
+type AlertProps = {
+  isDefaultShown: boolean;
+  timeout: number;
+  type: string;
+  message: string;
+};
+
+const Alert: FC<AlertProps> = ({
+  isDefaultShown = false,
+  timeout = 250,
+  type,
+  message,
+}) => {
+  const [isShown, setIsShown] = React.useState(isDefaultShown);
+  const [isLeaving, setIsLeaving] = React.useState(false);
+
+  let timeoutId = null;
+
+  React.useEffect(() => {
+    setIsShown(isDefaultShown);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [isDefaultShown, timeout, timeoutId]);
+
+  const closeAlert = () => {
+    setIsLeaving(true);
+    timeoutId = setTimeout(() => {
+      setIsLeaving(false);
+      setIsShown(false);
+    }, timeout);
+  };
+
+  return (
+    isShown && (
+      <div
+        className={`alert ${type} ${isLeaving ? "leaving" : ""}`}
+        role="alert"
+      >
+        <button className="close" onClick={closeAlert} />
+        {message}
+      </div>
+    )
+  );
+};
+
+export default Alert;
